32b0d96e42702c812428e475b0c2abd754ac508c
Szymon Poltorak <szymonpotorak@gmail.com>
2022-05-16 09:31:19 +0200

    Mala poprawka kodu

--- src/main/java/pl/edu/pw/ee/queries/Main.java
+++ src/main/java/pl/edu/pw/ee/queries/Main.java
@@ -1,13 +1,18 @@
 package pl.edu.pw.ee.queries;
 
+import java.io.IOException;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         try (var in = new Scanner(System.in)) {
             System.out.print("Please give me a number of test cases: ");
             int tests = in.nextInt();
+
+            if (tests < 1){
+                throw new IllegalArgumentException("How number of tests can be less than one ?");
+            }
 
             for (int t = 0; t < tests; t++) {
                 System.out.printf("%nTest[%d]:%n", t);
@@ -16,6 +21,10 @@ public class Main {
 
                 System.out.print("Please enter number of queries: ");
                 int numOfQueries = in.nextInt();
+
+                if (numOfQueries < 1 || numOfCandies < 1){
+                    throw new IllegalArgumentException("Why would anyone like to have less candies and queries than 1?");
+                }
 
                 System.out.println("Please enter " + numOfCandies + " sugar quantities: ");
                 var candies = new int[numOfCandies];
